fbce6b12622afd4f5770efb0b449e5faba1cb613
fsznajderman <fabszn@gmail.com>
2013-02-18 17:02:23 +0100

    fix version

--- src/main/assembly/resources/bin/launch.sh
+++ src/main/assembly/resources/bin/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar AppsWebMonitoring-1.0-final.jar   conf/conf.properties web/AppsWebMonitoring.html
+java -jar AppsWebMonitoring-1.0-final.jar conf/conf.properties web/AppsWebMonitoring.html
