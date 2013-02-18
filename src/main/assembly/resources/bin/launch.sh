2547b91ac65bf1ecd2421ff037adc9baf9b26108
fsznajderman <fabszn@gmail.com>
2013-02-18 16:35:09 +0100

    fix issue regarding jar file name

--- src/main/assembly/resources/bin/launch.sh
+++ src/main/assembly/resources/bin/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar AppsWebMonitoring-1.0-SNAPSHOT.jar   conf/conf.properties web/AppsWebMonitoring.htmljava
+java -jar AppsWebMonitoring-1.0-final.jar   conf/conf.properties web/AppsWebMonitoring.html
